26485fe24bf191c3a338409c133549823d6621ab
Riya <tly@mobagel.com>
2015-05-14 17:03:35 +0800

    p

--- myapp.js
+++ myapp.js
@@ -20,15 +20,36 @@ var skycons = new Skycons();
 /*
 Get value from Bootstrap dropdown menu
 */
+   
 $('#dropdown li').on('click', function(){
-    alert($(this).text());
+    //alert($(this).text());
+	var city=$(this).text();
+	       $.ajax('https://query.yahooapis.com/v1/public/yql', {
+	         method: 'GET',
+	         data: {
+	           q: 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + city + '")',
+	           format: 'json'
+	         },
+	         success: function (data) {
+				 var weatherInfo = data.query.results.channel;
+	             var cityMatch = 'It seems ' + weatherInfo.item.condition.text.toLowerCase()+' in '+ weatherInfo.location.city + ', ' + weatherInfo.location.country
+
+			   console.log(weatherInfo);
+	           $('.temperature').text(weatherInfo.astronomy.sunrise);
+			   //console.log(cityMatch);
+			   //console.log(nocity);
+	         }
+	       });
 });
 
-$.getJSON('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202306180&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',{},function(json) {
-  var w_code = weather_con[json.query.results.channel.item.condition.code.toString()];
-  var w_temp = Math.round((json.query.results.channel.item.condition.temp - 32)*5/9) + "℃";
-  console.log(json);
-  console.log(w_code);
-  console.log(w_temp);
-});
-$('#test').text(w_temp);
+
+
+
+// $.getJSON('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202306180&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',{},function(json) {
+//   var w_code = weather_con[json.query.results.channel.item.condition.code.toString()];
+//   var w_temp = Math.round((json.query.results.channel.item.condition.temp - 32)*5/9) + "℃";
+//   console.log(json);
+//   console.log(w_code);
+//   console.log(w_temp);
+// });
+// $('#test').text(w_temp);
