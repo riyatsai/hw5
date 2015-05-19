fd5a03ddf2432dd8a33517238d63667fa40805ba
Riya <tly@mobagel.com>
2015-05-20 04:34:35 +0800

    QQ

--- myapp.js
+++ myapp.js
@@ -40,7 +40,21 @@ Get value from Bootstrap dropdown menu
 		 $('.date1tem').text(((weatherInfo.item.forecast[1].low-32)*5/9).toFixed(0)+'-'+((weatherInfo.item.forecast[3].high-32)*5/9).toFixed(0));
 		 $('.date2tem').text(((weatherInfo.item.forecast[2].low-32)*5/9).toFixed(0)+'-'+((weatherInfo.item.forecast[3].high-32)*5/9).toFixed(0));
 		 $('.date3tem').text(((weatherInfo.item.forecast[3].low-32)*5/9).toFixed(0)+'-'+((weatherInfo.item.forecast[3].high-32)*5/9).toFixed(0));		 
-        }
+		 for (i = 0; i < 4; i++){ 
+		  if(weatherInfo.item.forecast[i].text==="Heavy Rain"||weatherInfo.item.forecast[i].text==="Rain"){
+		 	  skycons.set("icon"+i,Skycons.SLEET);
+		  }else if(weatherInfo.item.forecast[i].text==="PM Thunderstorms"||weatherInfo.item.forecast[i].text==="Heavy Thunderstorms"||weatherInfo.item.forecast[i].text==="Thunderstorms"||weatherInfo.item.forecast[i].text==="Thundershowers/Wind"||weatherInfo.item.forecast[i].text==="Showers"||weatherInfo.item.forecast[i].text==="AM Showers"){
+		 	  skycons.set("icon"+i,Skycons.RAIN);
+		  }else if(weatherInfo.item.forecast[i].text==="Clear"||weatherInfo.item.forecast[i].text==="Sunny"){
+		 	  skycons.set("icon"+i,Skycons.CLEAR_DAY);
+		  }if(weatherInfo.item.forecast[i].text==="Partly Cloudy"||weatherInfo.item.forecast[i].text==="AM Clouds/PM Sun"){
+		 	  skycons.set("icon"+i,Skycons.CLOUDY);
+		  }
+	     }
+		 skycons.play();
+		
+		
+		}
       });
 
   });
